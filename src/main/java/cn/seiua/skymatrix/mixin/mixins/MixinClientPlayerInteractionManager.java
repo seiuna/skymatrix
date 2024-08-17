bb2c9cdef53b2bedd335db79489726bb55a46112
seiuna <nmmlwc@gmail.com>
2024-08-17 17:59:26 +0800

    fix: autofish 不会拉杆

--- src/main/java/cn/seiua/skymatrix/mixin/mixins/MixinClientPlayerInteractionManager.java
+++ src/main/java/cn/seiua/skymatrix/mixin/mixins/MixinClientPlayerInteractionManager.java
@@ -18,6 +18,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(ClientPlayerInteractionManager.class)
 public class MixinClientPlayerInteractionManager {
 
+    @Shadow
+    private GameMode gameMode;
+
+    @Inject(at = @At("HEAD"), method = "interactItem")
+    public void onInteractItem(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        InteractItemEvent event = new InteractItemEvent(player.getStackInHand(hand), hand);
+        event.call();
+        InteractItemMEvent event1 = new InteractItemMEvent(player.getStackInHand(hand), hand, "HEAD");
+        event1.call();
+
+        if (event.isCancelled()) cir.cancel();
+    }
+
+    @Inject(at = @At("RETURN"), method = "interactItem")
+    public void onInteractItemPost(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> cir) {
+        InteractItemMEvent event = new InteractItemMEvent(player.getStackInHand(hand), hand, "RETURN");
+        event.call();
+
+        if (event.isCancelled()) cir.cancel();
+    }
 
 
 
